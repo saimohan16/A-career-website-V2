60c0d7ae7dc40bc5e0b3c47bb7d83b9f7dfd6140
sai1607 <sai.mohan1607@gmail.com>
2023-03-27 17:47:46 +0530

    added style and static folder

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 # Flask is a class inside the flask 
-from flask import Flask
+from flask import Flask, render_template
 # creating an app object of the class Flask 
 app = Flask(__name__)
 # @-is a decorator
 @app.route("/")
 def hello():
-    return "namaste sai anna"
+    #return "namaste sai anna"
+    return render_template('home.html')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True) 
+    app.run(host='0.0.0.0', debug=True)
+    
     
